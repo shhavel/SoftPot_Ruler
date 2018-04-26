25a980f8a3f7ee488adab597d7dcbc9f0d7cc07f
Oleksandr Avoyants <shhavel@gmail.com>
2018-04-26 18:28:26 +0300

    Add button

--- SoftPot_Ruler_Keyboard.ino
+++ SoftPot_Ruler_Keyboard.ino
@@ -1,28 +1,33 @@
 #include <Keyboard.h>
 
+#define BUTTON 7
 #define SOFT_POT_PIN A0 // Pin connected to softpot wiper
 #define RULER_OFFSET 15 // The point (value in inches) on the ruler where softpot starts.
 #define SOFT_POT_LENGTH 200 // Softpot length in tenths (1 tenth = 1/10 inch)
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(BUTTON, INPUT);
   pinMode(SOFT_POT_PIN, INPUT);
+  Serial.begin(9600); // debug
   Keyboard.begin();
 }
 
 void loop() {
-  // Read in the soft pot's ADC value
-  int softPotADC = analogRead(SOFT_POT_PIN);
+  if (digitalRead(BUTTON) == HIGH) {
+    // Read in the soft pot's ADC value
+    int softPotADC = analogRead(SOFT_POT_PIN);
 
-  if (softPotADC > 0) {
-    // Map the 0-1023 value to 0-SOFT_POT_LENGTH
-    float d = map(softPotADC, 0, 1023, 0, SOFT_POT_LENGTH);
+    if (softPotADC > 0)  {
+      // Map the 0-1023 value to 0-SOFT_POT_LENGTH
+      float d = map(softPotADC, 0, 1023, 0, SOFT_POT_LENGTH);
 
-    float l = RULER_OFFSET + (d / 10);
-    Serial.println(String(l) + " inches");
+      float l = RULER_OFFSET + (d / 10);
 
-    Keyboard.print(l);
+      Serial.println(String(l) + " inches"); // debug
+
+      Keyboard.print(l);
+    }
+
+    delay(500);
   }
-
-  delay(500);
 }
